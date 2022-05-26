af572cb3308a8402bc586661f0e453e773abea6b
Elijah <89745722+ElijahTheRandom@users.noreply.github.com>
2022-05-26 16:57:26 -0500

    Add files via upload

---
+++ acorn.js
@@ -0,0 +1,6 @@
+function acornify(){
+    let a = document.getElementById("acorn1");
+    a.style.filter = "grayscale(100%)";
+    let b = document.getElementById("acorn2");
+    b.style.filter = "grayscale(100%)";
+}
