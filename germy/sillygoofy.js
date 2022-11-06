70d6d86e439d8f30e158396aca6854f283aa99fe
Elijah <89745722+ElijahTheRandom@users.noreply.github.com>
2022-11-06 11:27:52 -0600

    brother birthday files

---
+++ germy/sillygoofy.js
@@ -0,0 +1,6 @@
+function chicken() {
+    let x = document.getElementById("chicken").value;
+    if (x == "banana") {
+        window.open("https://elijahtherandom.github.io/germy/beep.html", "_blank");
+    }
+  }
